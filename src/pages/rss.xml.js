7cb9022fe84fe35f3ef1a0c4c0691977ee6356e4
manuel peña <manpenap@gmail.com>
2023-11-08 21:44:12 -0300

    add rss

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,9 +1,12 @@
 import rss from '@astrojs/rss' 
+import { getCollection } from 'astro:content';
 
 
 
 export async function GET(context) {
-    const blog = await getCollection('blog');
+    const blog = await getCollection("blog", ({ data }) => {
+        return !data.draft && data.publishDate < new Date();
+      });
     return rss({
       title: 'Blog Clic Exitoso',
       description: 'Acá entregamos de Landing Pages y Marketing Digital',
